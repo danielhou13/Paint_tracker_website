063ad2ebe59f9108b18915b93b84e9edb621d494
danielhou13 <danielhou13@gmail.com>
2024-03-24 02:26:33 -0700

    Add simple alert message to let know that only adam can access system admin menu

--- paint-tracker-frontend/src/components/Home/Home.tsx
+++ paint-tracker-frontend/src/components/Home/Home.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import KanbanBoard from "../KanbanBoard/KanbanBoard";
 
 export default function Home({ permissions, loginFunction, user }) {
+  const [notAdam, setnotAdam] = useState(false);
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
           <span className="navbar-brand">Paint Availability Kanban Board</span>
           <ul className="navbar-nav">
-            <li className="navbar-text">Current User: {user}</li>
+            {user === "Adam" ? (
+              <li className="nav-item active">
+                <a
+                  className="nav-link px-2"
+                  href="https://django-paint-6d3cee377c88.herokuapp.com/admin/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  System admin menu
+                </a>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <button className="nav-link" onClick={() => setnotAdam(true)}>
+                  System admin menu
+                </button>
+              </li>
+            )}
+
+            <li className="navbar-text px-2">Current User: {user}</li>
             <li className="nav-item">
               <button className="nav-link" onClick={() => loginFunction(false)}>
                 Logout
@@ -21,6 +42,21 @@ export default function Home({ permissions, loginFunction, user }) {
         permissions={permissions}
         loginFunction={loginFunction}
       ></KanbanBoard>
+      {notAdam && (
+        <div
+          className="alert alert-warning alert-dismissible fade show"
+          role="alert"
+        >
+          You do not have authorization to access the System Admin menu.
+          Currently only Adam has authorization.
+          <button
+            type="button"
+            className="btn-close"
+            data-dismiss="alert"
+            onClick={() => setnotAdam(false)}
+          ></button>
+        </div>
+      )}
       <footer className="fixed-bottom mt-auto py-2 bg-light">
         All users except for John can edit the stock of paints. The system admin
         Adam can update the permissions and assigned groups of each user to
